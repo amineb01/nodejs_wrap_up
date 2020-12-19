e6201ab8549a05b47227229dd48241f1b893e5bd
bellarej amine <bellarej.amine@gmail.com>
2020-12-19 17:58:58 +0100

    set up docker && docker-compose

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ dotenv.config();
 
 const hostname = process.env.SERVER || '127.0.0.1';
 const port = process.env.PORT || 3000;
-
+app.get('/', (req, res) =>{
+    res.send("it's working :)")
+})
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
